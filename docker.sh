4429656e268c3e65819da43b7cd65155acb7173b
Sandeepgompa <sandeepgompa01@gmail.com>
2025-03-10 19:44:09 +0530

    docker.sh eks updated

--- docker.sh
+++ docker.sh
@@ -19,6 +19,8 @@ chmod +x ./kubectl
 mv kubectl /usr/local/bin/kubectl
 
 #eksctl
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
 tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 mv /tmp/eksctl /usr/local/bin
